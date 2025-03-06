d0f945a9e0706d1653b76deb280b9007e807bdc9
danialjankouk <dani.jankook@gmail.com>
2025-03-06 12:31:05 +0330

    add test

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-
 import { Outlet } from "react-router";
 import "./App.css";
-import Navbar from './components/Navbar'
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <>
-      <Navbar/>
-      <Outlet/>
+      <Navbar />
+      <Outlet />
       <h1 className="text-white">Hiiii</h1>
     </>
   );
