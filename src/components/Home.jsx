359a25eae5d9e716785f1cd3278e691048bb204f
danialjankouk <dani.jankook@gmail.com>
2025-03-06 23:38:50 +0330

    home page logic

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
-import { getPopularMovies } from '../services/api';
+import React from "react";
+import { getPopularMovies } from "../services/api";
+import { useState, useEffect } from "react";
 const Home = () => {
-    return (
-        <div>
-            <p className='text-white'>byeeee</p>
-        </div>
-    );
+  const [movie, setMovie] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [err, setErr] = useState(null);
+
+  useEffect(() => {
+    const loadPopularMovie = async () => {
+      try {
+        const response = await getPopularMovies();
+        setMovie(response);
+      } catch (error) {
+        console.log(error);
+        setErr("faild to load");
+      }
+    };
+    loadPopularMovie();
+  }, []);
+  console.log(movie);
+
+  return (
+    <div>
+      <h1>Popular Movies</h1>
+      {loading ? (
+        <div>Loading ...</div>
+      ) : (
+        <div>{movie.map((data) => data)}</div>
+      )}
+    </div>
+  );
 };
 
 export default Home;
