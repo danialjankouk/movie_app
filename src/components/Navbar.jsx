8be3ed50a8f141c12101dbf030d4d4dff03137c4
niloufar <niloufarrezvani80@gmail.com>
2025-03-07 00:23:22 +0330

    movie_card

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,11 @@ import './Navbar.css'
 import { NavLink } from 'react-router'
 function Navbar() {
   return (
-    <nav className="flex justify-around gap-10 itmes-center  h-20 background-my shadow-2xl">
+    <nav className="flex justify-between px-[5%] gap-10 items-center  h-20 my-background shadow-2xl">
          
-        <div className="text-xl text-blue-300 flex justify-center  items-center"> movie app</div>
+        <div className="text-xl text-blue-300 flex justify-center  items-center cursor-pointer"> movie app</div>
         <div className="flex gap-10 justify-center items-center text-blue-300">
-            <NavLink to='/' className="text-xl">Home</NavLink>
+            <NavLink to='/home' className="text-xl">Home</NavLink>
             <NavLink to='/favorite' className="text-xl" >Favorite movie</NavLink>
         </div>
     </nav>
