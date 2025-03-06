64ac65dcac7057b370144c697916b2183861f581
danialjankouk <dani.jankook@gmail.com>
2025-03-07 01:00:24 +0330

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
       <Navbar />
       <Outlet />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </>
   );
